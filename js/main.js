d9e5fea82061c306e11ace449cfa58a25ee71929
Krystal Furlani <krystal@Krystals-MacBook-Pro.local>
2022-04-10 16:32:14 -0400

    not the result I was hoping for

--- js/main.js
+++ js/main.js
@@ -4,10 +4,30 @@
 
 let bttn = document.querySelector('#popbttn'),
     showPop = document.getElementById('#popup');
-    
+
+// trying to talk this out. in my css i should have an id that has a display of none or visibility of none 
+// and then a second class with visible or display block (like we did with GOT)
+// i think i need to do something like 'appendChild'
+// create variable (like the let statement) then create array, create function and add event listener. 
+// I almost understand each individual step but then when I need to add them all together I get the wrong answer. 
+// i know this looks like i haven't done much but i've tried and then deleted multiple attempts. 
+
+
+const popMsgData = [
+    'HYDRAGLYDE IS OUR ULTRA MOISTURIZING FORMULA THAT HELPS KEEP YOU AND YOUR CONTACTS FEELING FRESH'
+   
+]
+
 
 function openPopup()
     showPop.classList.add('.open-popup');
+
+    let popMsgData = showPop.getElementsByClassName(".popup")
+
+    //not right.. had to delete class/id from html since it was messing that up. 
+
+
+
 
 
 
